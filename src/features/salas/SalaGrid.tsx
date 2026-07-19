585057cba1048ca6dc7981659203e266d91332d6
JavierCastillo <castillojavi243@gmail.com>
2026-07-19 15:19:41 -0600

    Ajustar responsividad de tabla de salas

--- src/features/salas/SalaGrid.tsx
+++ src/features/salas/SalaGrid.tsx
@@ -2,7 +2,7 @@
 
 import { salasData } from "@/data/salasData";
 import { useAppDispatch } from "@/redux/hooks";
-import { SquarePen, SquareX} from "lucide-react";
+import { SquarePen, SquareX } from "lucide-react";
 
 import styles from "@/features/salas/salas.module.css"
 
@@ -10,33 +10,35 @@ export default function ProductGrid() {
     const dispatch = useAppDispatch();
 
     return (
-        <table className={styles.salasTable}>
-            <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Nombre</th>
-                    <th>Filas</th>
-                    <th>Columnas</th>
-                    <th>Capacidad</th>
-                    <th>Acciones</th>
-                </tr>
-            </thead>
-
-            <tbody>
-                {salasData.map((sala) => (
-                    <tr key={sala.id}>
-                        <td>{sala.id}</td>
-                        <td>{sala.nombre}</td>
-                        <td>{sala.capacidad.filas}</td>
-                        <td>{sala.capacidad.columnas}</td>
-                        <td>{sala.capacidad.filas * sala.capacidad.columnas}</td>
-                        <td className={styles.actions}>
-                            <button className={styles.iconButton}><SquarePen/></button>
-                            <button className={styles.iconButton}><SquareX/></button>
-                        </td>
+        <div className="tableContainer">
+            <table className={styles.salasTable}>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Nombre</th>
+                        <th>Filas</th>
+                        <th>Columnas</th>
+                        <th>Capacidad</th>
+                        <th>Acciones</th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+
+                <tbody>
+                    {salasData.map((sala) => (
+                        <tr key={sala.id}>
+                            <td>{sala.id}</td>
+                            <td>{sala.nombre}</td>
+                            <td>{sala.capacidad.filas}</td>
+                            <td>{sala.capacidad.columnas}</td>
+                            <td>{sala.capacidad.filas * sala.capacidad.columnas}</td>
+                            <td className={styles.actions}>
+                                <button className={styles.iconButton}><SquarePen /></button>
+                                <button className={styles.iconButton}><SquareX /></button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
